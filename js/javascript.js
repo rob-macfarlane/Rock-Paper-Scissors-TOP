e79342858821d800deaff1fc0d1113e7ca1472a9
rob-macfarlane <remacfarlane29@gmail.com>
2022-09-11 21:05:14 -0700

    Update getComputerChoice and playerSelection functions

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,47 @@
-console.log('Hello World!')
+
+
+
+function getComputerChoice() {
+
+    let choiceInteger = Math.floor(Math.random() * 3);
+    let computerChoice;
+    switch(choiceInteger) {
+        case 0: 
+            computerChoice = 'rock';
+            break;
+        case 1: 
+            computerChoice = 'paper';
+            break;
+        case 2: 
+            computerChoice = 'scissors';
+            break;
+
+    return computerChoice;
+    }
+}
+
+getComputerChoice();
+
+
+
+// create a function to get the players selection
+
+function getPlayerChoice() {
+
+    //input 
+    let userInput = prompt("Rock Paper or scissors?", 'Rock');
+    userInput = userInput.toLowerCase();
+
+    //return the player selection
+    return userInput;
+    
+}
+
+getPlayerChoice();
+
+// create a function that plays a round of rock paper scissors with computer and player selections as inputs
+
+    //if statements for the 6 cases
+
+
+    // return the string for the loser/ winner case
